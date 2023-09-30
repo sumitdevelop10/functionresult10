23b062d0f46080e2a4b5099fa8ba0d8059d29ba2
sumit <sumitmessi018@gmail.com>
2023-09-30 16:43:47 +0530

    eighth

---
+++ function10.js
@@ -0,0 +1,20 @@
+function addTwoNumbers(number1 , number2){
+     
+    let result = number1 + number2
+     console.log("champion")
+     return number1 + number2
+ }
+
+addTwoNumbers(3 ,4)
+
+const result = addTwoNumbers(20 ,30)
+
+console.log("result :" , result);
+
+function loginUserMessage(username) {
+    return `$ {username} just logged in`
+    
+}
+
+console.log(loginUserMessage("sumit"))
+
